59ac61f1c61fac52a1dc856f686c2430fffaf813
Stefano Pozzati <pozz.ste@gmail.com>
2025-04-18 14:33:03 +0200

    wip calendar fetching

--- tests/test_calendar.py
+++ tests/test_calendar.py
@@ -1,6 +1,11 @@
 from event_tracking.components.calendar import fetch_calendar_events
 
 
+def test_credentials():
+
+
+    print("true")
+
 def test_fetch_calendar_events():
     events = fetch_calendar_events(30)
 
